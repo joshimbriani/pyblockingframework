14ca3136eae4d8912e1f14328ef91500b2539dbc
Josh Imbriani <joshimbriani@gmail.com>
2016-03-28 12:04:48 -0500

    Added data and basic structure

---
+++ comparer.py
@@ -0,0 +1,12 @@
+class Comparer:
+
+	data1 = []
+	data2 = []
+	comparisonMethod = ""
+
+	def __init__(self, data1, data2, comparisonMethod):
+		self.data1 = data1
+		self.data2 = data2
+		comparisonMethod = comparisonMethod
+
+		
---
+++ runner.py
@@ -0,0 +1,28 @@
+import csv
+import sys
+
+def main():
+	file1Name = sys.argv[1]
+	file2Name = sys.argv[2]
+	comparisonToRun = sys.argv[3]
+	
+	data1 = []
+	data2 = []
+	
+	with open(file1Name, 'rb') as csvfile:
+		fileReader = csv.reader(csvfile)
+		for row in fileReader:
+			#print row
+			data1.append(row)
+
+	with open(file2Name, 'rb') as csvfile:
+		fileReader = csv.reader(csvfile)
+		for row in fileReader:
+			data2.append(row)
+
+	print len(data1)
+	print len(data2)
+	print comparisonToRun
+
+if __name__ == "__main__":
+	main()
