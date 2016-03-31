d895a17d33b55ccc979e706d332261643bd9538f
Josh Imbriani <joshimbriani@gmail.com>
2016-03-30 23:38:01 -0500

    Added total description

--- totalDescription.py
+++ totalDescription.py
@@ -1,2 +1,13 @@
+import urlparse
+
 def totalDescription(data1, data2):
-	return []
+	#implementation of infix from URI in python
+
+	#loop over file to extract strings between backslashes and store in list
+	p = urlparse.urlsplit('http://dblp.13s.de/d2r/resource/publications/books/sp/wooldridgeV99/ThalmannN99').path
+	#p = /d2r/resource/publications/books/sp/wooldridgeV99/ThalmannN99
+
+	splitted = p.split('/')
+	print splitted
+	for i in splitted:
+    		print i
