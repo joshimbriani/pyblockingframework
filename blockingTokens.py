8e5e956649061dccf8886bd9abc95a7db47835ea
Josh Imbriani <joshimbriani@gmail.com>
2016-05-02 21:24:47 -0500

    Fixed scoring

--- blockingTokens.py
+++ blockingTokens.py
@@ -1,18 +1,18 @@
 from entityProfile import EntityProfile
 from blocks import Block
 
-def blockingTokens(data1, data2):
+def blockingTokens(data1, data2, mostImportant):
 
 	blocks = {}
 
 	for dataItem in data1 + data2:
-		for dataAttribute in dataItem:
-			wordList = dataAttribute.split(' ')
-			for word in wordList:
-				if word in blocks:
-					blocks[word].append(dataItem)
-				else:
-					blocks[word] = []
-					blocks[word].append(dataItem)
+		#for dataAttribute in dataItem:
+		wordList = dataItem[mostImportant].split(' ')
+		for word in wordList:
+			if word in blocks:
+				blocks[word].append(dataItem)
+			else:
+				blocks[word] = []
+				blocks[word].append(dataItem)
 
 	return blocks
--- comparer.py
+++ comparer.py
@@ -13,6 +13,11 @@ class Comparer:
 	def __init__(self, data1, data2, comparisonMethod):
 		self.data1 = data1
 		self.data2 = data2
+		
+		self.dataelement =  input("Which element of the first data set is most important?")
+
+		#self.data2element = input("Which element of the second data set is most important?")
+
 		comparisonMethod = comparisonMethod
 
 	def runTotalDescription(self):
@@ -21,7 +26,7 @@ class Comparer:
 		#print blocks
 
 	def runBlockingTokens(self):
-		self.blocks = blockingTokens(self.data1, self.data2)
+		self.blocks = blockingTokens(self.data1, self.data2, self.dataelement)
 		#print blocks
 
 	def runBSL(self, data1, data2):
@@ -30,6 +35,8 @@ class Comparer:
 
 	def evaluate(self, blocks, lenData1, lenData2):
 		totalComparisons = 0
+		correctComparisons = 0
+		allComparisons = 0
 		for block in self.blocks:
 			if len(self.blocks[block]) == 0:
 				totalComparisons += 0
@@ -39,9 +46,20 @@ class Comparer:
 		#print totalComparisons, lenData1, lenData2
 		RR = totalComparisons / (lenData1*lenData2)
 
-		for block in self.blocks:
-			for index, item in self.blocks[block].enumerate():
-				for i in range(index, len(self.blocks[block]):
-					fuzz.token_set_ratio(self.blocks[block][index][1], self.blocks[block][index][1])
+		print "RR is " + str(RR)
 
-		return RR
+		print "Starting PC calculation"
+
+		for index1, block in enumerate(self.blocks):
+			for index, item in enumerate(self.blocks[block]):
+				for i in range(index, len(self.blocks[block])):
+					if fuzz.token_set_ratio(self.blocks[block][index][1], self.blocks[block][i][1]) > 70:
+						correctComparisons += 1
+					allComparisons += 1
+			print "Block " + str(index1) + " of " + str(len(self.blocks)) + " compared"
+
+		PC = correctComparisons / allComparisons
+
+		print "PC is " + str(PC)
+
+		return (RR, PC)
